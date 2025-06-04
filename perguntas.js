1a286637e1fc1b2349f2e6b6b447da29e68919e4
tatiana-carmo <163299559+tatiana-carmo@users.noreply.github.com>
2025-06-04 09:19:34 -0300

    1

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual é o maior time do Brasil?',
+    'São Paulo'
+)
+
+criaCartao(
+    'astronomia',
+    'Qual é o nome do nosso satélite natural??',
+    'A Lua'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o símbolo químico da água?',
+    ' H₂O.'
+)
+
+criaCartao(
+    'História',
+    'Em que ano o Brasil proclamou sua independência?',
+    '1822'
+)
